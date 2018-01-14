3f324f5bbaa1c54722bc91f1c103a6e16584afa9
Kenzo <kenzoclauw@gmail.com>
2018-01-14 01:14:29 +0100

    final commit

--- TweetCollector/twitterstream.py
+++ TweetCollector/twitterstream.py
@@ -2,6 +2,11 @@ import oauth2 as oauth
 import urllib2 as urllib
 from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
+from twokenize import tokenize
+from emoticons import analyze_tweet
+import argparse
+import twokenize
+
 import re
 import csv
 import json
@@ -53,28 +58,22 @@ def twitterreq(url, method, parameters):
 
   return response
 
-
-def extract_hash_tags(s):
-  return re.findall(r"#(\w+)", s)
-  #return set([re.sub(r"#+", "#", k) for k in set([re.sub(r"(\W+)$", "", j, flags = re.UNICODE) for j in set([i for i in text.split() if i.startswith("#")])])])
-
-
-
 def fetchsamples(limit):
   
   tweets = []
-
   url = "https://stream.twitter.com/1.1/statuses/filter.json?locations=-122.995004,32.323198,-67.799695,49.893813&lang=en"
-  #url = "http://stream.twitter.com/1.1/statuses/filter.json?locations=-122.75,36.8,-121.75,37.8,-74,40,-73,41"
   parameters = []
   response = twitterreq(url, "GET", parameters)
 
   tweets = []
-
+  it=0
+  #Fetch sample as a JSON object
   for index,line in enumerate(response):
-
-    #print(line.strip())
-    tweet = json.loads(line.strip())["text"]
+    tweet_object = json.loads(line.strip())
+    if 'text' in tweet_object:
+      tweet = json.loads(line.strip())["text"]
+      it+=1
+      print(tweet)
     tweets.append(tweet)
     if index == limit+1:
         break
@@ -82,55 +81,78 @@ def fetchsamples(limit):
   return tweets
    
 
-
-def preProcess(tweets):
-  #Pre-processing
-  #Remove the non-character symbols
-  tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
-  #tweets = [tknzr.tokenize(item.lower()) for item in tweets]
-
-  tweets = [" ".join(re.findall("[a-zA-Z]+", word)) for word in tweets]
-  #tweets = [tknzr.tokenize(item.lower()) for item in tweets]
-
-  #special = ['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}','#']
-  #determiners = ["the","a","an","another","for","nor","but","or","yet","so","in","under","towards","before"]
-  #stop = set(stopwords.words('english'))
- # stop.update(determiners)
- # stop.update(special) 
- # tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
-  #tweets = [tknzr.tokenize(item.lower()) for item in tweets]
-  return tweets
-
-def extractTweets(tweets):
+#Extract the hashtags from the text
+def extractHashtags(tweets):
   hashtags = []
   for sentence in tweets:
     for word in sentence.split():
-      #print(word)
       if word[0] == "#":
         filtered_tag = " ".join(re.findall("[a-zA-Z]+", word))
-        hashtags.append(filtered_tag)
-    #print(extract_hash_tags(sentence))
-    #hashtags.append(extract_hash_tags(sentence))
+        hashtags.append(filtered_tag.lower())
 
   return hashtags
+
+#Tokenize the tweets and remove words containing hashtags or httptags
+def tokenizeTweets(tweets):
+    total_tweets = []
+    filter_prefix_set = ('@', 'http', 'www')
+    # filter for english
+    for status in tweets:
+        tokenized = tokenize(status)
+        # remove http tags and hashtags
+        words = [re.sub(r'[^\w\s]','', word).lower() for word in tokenized if not word.startswith(filter_prefix_set)]
+        if words:
+            total_tweets.append(words)
+
+    return total_tweets
+
+
+#Preprocess the data by removing stop words and determiners
+def processTweets(tweets):
+
+    determiners = ["the","a","an","another","for","nor","but","or","yet","so","in","under","towards","before","or","im"]
+    stop = set(stopwords.words('english'))
+    stop.update(determiners)
+    words = []
+    for tweet in tweets:
+      for word in tweet:
+        if not word in stop and len(word) > 1:
+          words += words
+
+    return words
+
+
+
 
 
 if __name__ == '__main__':
 
-  #Retrieve tweets
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--n', type=int,
+    help='The amount of tweets to be received')
 
-  tweets = fetchsamples(5)
-  #print(tweets)
-  #print(tweets)
-  #print()
-  tweets = preProcess(tweets)
-  hashtags = extractTweets(tweets) 
 
+  #Retrieve the raw tweets
+  tweets = fetchsamples(parser.parse_args().n)  
+
+  #Extract the hashtags from the tweets
+  hashtags = extractHashtags(tweets) 
+
+  #Tokenize the tweets
+  tweets = tokenizeTweets(tweets)
+
+  #Preprocess
+  tweets = processTweets(tweets)
+
+  
+  #Write results to file
   with open('tweets.csv', 'wb') as tweetFile:
+
     for word in tweets:
-      print(word)
-#      wr = csv.writer(tweetFile)
-#      wr.writerow([word])
+      w = word.lower()
+      if w != "":
+        wr = csv.writer(tweetFile)
+        wr.writerow([word])
 
   with open('hashtags.csv', 'wb') as tagFile:
     for tag in hashtags:
